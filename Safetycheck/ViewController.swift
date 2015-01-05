bab21e42ffe63a117a1efc2e5ed49901f36dfb54
Martin Lok <martinlok@Martins-MacBook-Pro-2.local>
2015-01-05 13:54:26 +0100

    Initial Commit

---
+++ Safetycheck/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Safetycheck
+//
+//  Created by Martin Lok on 05/01/15.
+//  Copyright (c) 2015 Martin Lok. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
